023710032a8ca29b7e279073d11443161404bd7d
jakub <g00408184@atu.ie>
2022-12-12 10:24:19 +0000

    Getter and setters

---
+++ src/main/java/ie/atu/TrawlerApp.java
@@ -0,0 +1,31 @@
+package ie.atu;
+
+public class TrawlerApp {
+    private String name;
+    private int transponderid;
+    private String type;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTransponderid() {
+        return transponderid;
+    }
+
+    public void setTransponderid(int transponderid) {
+        this.transponderid = transponderid;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
